9202ed2a6ccddc3b0f436a42fe9864819f01fd84
CocoZohan <dos93@mail.ru>
2019-10-14 21:18:29 +0600

    j

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json	| jq -r --arg ID $HERO_ID '.[] | select(.id == ($ID|tonumber)) | .connections.relatives'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json	| jq -j --arg ID $HERO_ID '.[] | select(.id == ($ID|tonumber)) | .connections.relatives'
