f2ec61f608fe3d6d60340fe2d4dd535cf93f21da
CocoZohan <dos93@mail.ru>
2019-10-15 16:54:27 +0600

    myfamily fix

---
+++ printcombn.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "fmt"
+
+func PrintCombN(n int) {
+	a := make([]int, n)
+
+	for i, _ := range a {
+		a[i] = i
+	}
+
+	index := n - 1
+
+	for ; ; {
+		for _,v := range a {
+			fmt.Printf("%d", v)
+		}
+	}
+}
