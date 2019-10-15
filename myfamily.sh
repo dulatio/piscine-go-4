50811663a862fe3e62a7dd51a1ba06d5313a7b80
CocoZohan <dos93@mail.ru>
2019-10-15 16:57:34 +0600

    myfamily fix

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json	| jq --arg ID $HERO_ID '.[] | select(.id == ($ID|tonumber)) | .connections.relatives'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json	| jq --arg ID $HERO_ID '.[] | select(.id == ($ID|tonumber)) | .connections.relatives' | sed 's/"//g'
