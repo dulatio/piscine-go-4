5fd1b6a4421246a933b3a3e86b3ae9d4a3d9a8bb
CocoZohan <dos93@mail.ru>
2019-10-16 13:06:29 +0600

    print comb

--- printcomb.go
+++ printcomb.go
@@ -1,17 +1,20 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func PrintComb(){
-	for i := 0; i <= 7; i++ {
-		for j := i+1; j <= 8; j++ {
-			for k := j+1; k <= 9; k++ {
-				fmt.Printf("%d%d%d", i, j, k)
-				if(i != 7 || j != 8 || k != 9){
-					fmt.Printf(", ")
+	for i := '0'; i <= '7'; i++ {
+		for j := i+1; j <= '8'; j++ {
+			for k := j+1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+				if(i != '7' || j != '8' || k != '9'){
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
 			}
 		}
 	}
-	fmt.Println()
+	z01.PrintRune(10)
 }
--- test/test.go
+++ test/test.go
@@ -1,16 +1,8 @@
 package main
 
-import (
-	"fmt"
-	piscine ".."
-)
+import piscine ".."
+
 
 func main(){
-	a := 13
-	b := 2
-	var div int
-	var mod int
-	piscine.DivMod(a,b,&div, &mod)
-	fmt.Println(div)
-	fmt.Println(mod)
+	piscine.PrintComb()
 }
