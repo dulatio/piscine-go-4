6602263955103aa369a714303eead16c29e1426e
CocoZohan <dos93@mail.ru>
2019-10-16 13:14:10 +0600

    comb2

--- printcomb2.go
+++ printcomb2.go
@@ -1,22 +1,27 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
-func PrintComb2(){
-	for i1 := 0; i1 <= 9; i1++ {
-		for i2 := 0; i2 <= 9; i2++ {
-			for j1 := i1; j1 <= 9; j1++{
-				for j2 := 0; j2 <= 9; j2++{
-					if(i1 == j1 && i2 >= j2){
+func PrintComb2() {
+	for i1 := '0'; i1 <= '9'; i1++ {
+		for i2 := '0'; i2 <= '9'; i2++ {
+			for j1 := i1; j1 <= '9'; j1++ {
+				for j2 := '0'; j2 <= '9'; j2++ {
+					if i1 == j1 && i2 >= j2 {
 					} else {
-						fmt.Printf("%d%d %d%d", i1, i2, j1, j2)
-						if(i1 != 9 || i2 != 8 || j1 != 9 || j2 != 9){
-							fmt.Printf(", ")
+						z01.PrintRune(i1)
+						z01.PrintRune(i2)
+						z01.PrintRune(' ')
+						z01.PrintRune(j1)
+						z01.PrintRune(j2)
+						if i1 != '9' || i2 != '8' || j1 != '9' || j2 != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
 						}
 					}
 				}
 			}
 		}
 	}
-	fmt.Println()
+	z01.PrintRune(10)
 }
