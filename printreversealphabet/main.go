e45672c77c32297dbe7ba216ed4c29343fd926e9
CocoZohan <dos93@mail.ru>
2019-10-16 12:18:28 +0600

    still wrong

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,13 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	//for c := 'z'; c >= 'a'; c-- {
-	//	z01.PrintRune(c)
-	//}
-	//z01.PrintRune('\n');
-	//z01.PrintRune('\n');
-	alph := "zyxwvutsrqponmlkjihgfedcba\n"
-	for i := 0; i < 27; i++ {
-		z01.PrintRune(rune(alph[i]))
+	for c := 'z'; c >= 'a'; c-- {
+		z01.PrintRune(c)
 	}
+	z01.PrintRune(10);
 }
