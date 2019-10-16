9ddde47e540d036f0204e7f04b54c9aff45a026b
CocoZohan <dos93@mail.ru>
2019-10-16 12:06:09 +0600

    wring

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,9 +3,13 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for c := 'z'; c >= 'a'; c-- {
-		z01.PrintRune(c)
+	//for c := 'z'; c >= 'a'; c-- {
+	//	z01.PrintRune(c)
+	//}
+	//z01.PrintRune('\n');
+	//z01.PrintRune('\n');
+	alph := "zyxwvutsrqponmlkjihgfedcba\n"
+	for i := 0; i < 27; i++ {
+		z01.PrintRune(rune(alph[i]))
 	}
-	z01.PrintRune('\n');
-	z01.PrintRune('\n');
 }
