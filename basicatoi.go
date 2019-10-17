1770fed86c22fd05b1715e4080997c995a255d48
CocoZohan <dos93@mail.ru>
2019-10-17 16:03:57 +0600

    bruteforcing my way

--- basicatoi.go
+++ basicatoi.go
@@ -4,7 +4,27 @@ func BasicAtoi(s string) int {
 	var res int
 	for _, r := range s {
 		res *= 10
-		res += int(r) - 48
+		if r == 48 {
+			res += 0
+		} else if r == 49 {
+			res += 1
+		} else if r == 50 {
+			res += 2
+		} else if r == 51 {
+			res += 3
+		} else if r == 52 {
+			res += 4
+		} else if r == 53 {
+			res += 5
+		} else if r == 54 {
+			res += 6
+		} else if r == 55 {
+			res += 7
+		} else if r == 56 {
+			res += 8
+		} else if r == 57 {
+			res += 9
+		}				
 	}
 	return res
 }
--- test/test.go
+++ test/test.go
@@ -7,7 +7,7 @@ import (
 
 func main(){
 	str := "1"
-	str1 := "12"
+	str1 := "0000012"
 	str2 := "6431"
 	fmt.Println(piscine.BasicAtoi(str))
 	fmt.Println(piscine.BasicAtoi(str1))
