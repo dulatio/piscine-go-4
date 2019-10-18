5eccc0ea9707c2d3979bca75df4aa58ab681dbe7
CocoZohan <dos93@mail.ru>
2019-10-18 19:41:02 +0600

    finished raid1

--- test/test.go
+++ test/test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main(){
-	raid1.Raid1c(5,3)
-	raid1.Raid1c(5,1)
-	raid1.Raid1c(1,5)
-	raid1.Raid1c(0,3)
-	raid1.Raid1c(1,1)
-	raid1.Raid1c(2,2)
+	raid1.Raid1e(5,3)
+	raid1.Raid1e(5,1)
+	raid1.Raid1e(1,5)
+	raid1.Raid1e(0,3)
+	raid1.Raid1e(1,1)
+	raid1.Raid1e(2,2)
 	fmt.Println()
 }
