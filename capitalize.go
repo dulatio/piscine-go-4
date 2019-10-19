2a31b59f41514d744d703c788fee6f7012107bfd
CocoZohan <dos93@mail.ru>
2019-10-19 15:18:34 +0600

    ugly code

---
+++ capitalize.go
@@ -0,0 +1,34 @@
+package piscine
+
+func Capitalize(s string) string {
+	res := ""
+	flag := true
+	for _, r := range s {
+		if flag {
+			if r >= 'a' && r <= 'z' {
+				res += string(r + ('A' - 'a'))
+				flag = false
+			} else if (r >= '0' && r <= '9') || (r >= 'A' && r <= 'Z') {
+				res += string(r)
+				flag = false
+			} else if r == ' ' || r == '\t' {
+				res += string(r)
+			} else {
+				res += string(r)
+				flag = true
+			}
+		} else {
+			if r >= 'A' && r <= 'Z' {
+				res += string(r - ('A' - 'a'))
+			} else if (r >= '0' && r <= '9') || (r >= 'a' && r <= 'z') {
+				res += string(r)
+			} else if r == ' ' || r == '\t' {
+				res += string(r)
+			} else {
+				res += string(r)
+				flag = true
+			}
+		}
+	}
+	return res
+}
