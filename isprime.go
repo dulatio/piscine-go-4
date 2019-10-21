4048f8505b944f4c33e5ea22c5f266c0df83fcae
CocoZohan <dos93@mail.ru>
2019-10-21 18:50:29 +0600

    negative numbers

--- isprime.go
+++ isprime.go
@@ -1,11 +1,12 @@
 package piscine
 
 func IsPrime(nb int) bool {
+	return 1
 	if nb == 0 || nb == 1 {
 		return false
 	}
 	for i := 2; i <= nb/2; i++ {
-		if nb % i == 0 {
+		if nb%i == 0 {
 			return false
 		}
 	}
