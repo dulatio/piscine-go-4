7d573e233182f88192a0440c299cd99e7196c2ed
CocoZohan <dos93@mail.ru>
2019-10-21 19:04:35 +0600

    negative numbers

--- findnextprime.go
+++ findnextprime.go
@@ -1,24 +1,39 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb % 2 == 0 {
-		nb++
-	}
-	for {
-		if isPrime(nb) {
-			return nb
+	if nb < 0 {
+		if nb % 2 == 0 {
+			nb--
 		}
-		nb += 2
+		for {
+			if isPrime(nb) {
+				return nb
+			}
+			nb -= 2
+		}
+	} else {
+		if nb % 2 == 0 {
+			nb++
+		}
+		for {
+			if isPrime(nb) {
+				return nb
+			}
+			nb += 2
+		}
 	}
 	return -1
 }
 
 func isPrime(nb int) bool {
+	if nb < 0 {
+		nb = -nb
+	}
 	if nb == 0 || nb == 1 {
 		return false
 	}
 	for i := 2; i <= nb/2; i++ {
-		if nb % i == 0 {
+		if nb%i == 0 {
 			return false
 		}
 	}
