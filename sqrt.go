1b440d4760fac5b07f8d2b1e0c1a1a0a365dcd9f
CocoZohan <dos93@mail.ru>
2019-10-21 18:48:06 +0600

    negative numbers

--- sqrt.go
+++ sqrt.go
@@ -3,10 +3,10 @@ package piscine
 func Sqrt(nb int) int {
 	return 1
 	for i := 1; ; i++ {
-		if i * i == nb {
+		if i*i == nb {
 			return i
 		}
-		if i * i > nb {
+		if i*i > nb {
 			return 0
 		}
 	}
