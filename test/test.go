70f3d8f2629d4a97922966c8437917e6575053ed
CocoZohan <dos93@mail.ru>
2019-10-21 20:29:39 +0600

    stupid amnesiac

--- test/test.go
+++ test/test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main(){
-	fmt.Println(piscine.FindNextPrime(-185519))
-	fmt.Println(piscine.FindNextPrime(-528929))
-	fmt.Println(piscine.FindNextPrime(-578167))
+	piscine.EightQueens2()
+	fmt.Println()
 }
