b867e51610debed89f518dbd66108fb1f46107d1
CocoZohan <dos93@mail.ru>
2019-10-21 12:04:30 +0600

    big numbers bug

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,16 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	/*res := 1
+	if nb < 0 {
+		return 0
+	}
+	res := 1
 	for i := 1; i <= nb; i++ {
 		res *= i
+		if res < 0 {
+			return 0
+		}
 	}
-	return res*/
+	return res
 	return 1
 }
