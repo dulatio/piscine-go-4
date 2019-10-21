804ca66e2f4f53b8b92bd30bed06b53f8e5e7f77
CocoZohan <dos93@mail.ru>
2019-10-21 18:33:16 +0600

    big numbers

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -8,5 +8,10 @@ func RecursiveFactorial(nb int) int {
 	if nb < 2 {
 		return 1
 	}
-	return nb * RecursiveFactorial(nb-1)
+	res := nb * RecursiveFactorial(nb-1)
+	if res < 0 {
+		return 0
+	} else {
+		return res
+	}
 }
