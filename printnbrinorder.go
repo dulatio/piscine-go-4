b88db0d07c85bd6dc01593815f5651fa9c1ea5c9
cocozohan <dos93@mail.ru>
2019-10-22 11:20:07 +0600

    missing zero

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,7 +3,20 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
+	var ar []int
+	count := 0
 	for ; n > 0; n /= 10 {
-		z01.PrintRune(rune(n % 10))
+		ar = append(ar, n % 10)
+		count++
+	}
+	for i := 1; i < count; i++ {
+		for j := 0; j < count - i; j++ {
+			if ar[j] > ar[j+1] {
+				ar[j], ar[j+1] = ar[j+1], ar[j]
+			}
+		}
+	}
+	for i := 0; i < count; i++ {
+		z01.PrintRune(rune(ar[i]) + '0')
 	}
 }
--- test/test.go
+++ test/test.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-	piscine ".."
-)
+import piscine ".."
 
 func main() {
-	piscine.PrintNbrBase(125, "0123456789")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "01")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "0123456789ABCDEF")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(-125, "choumi")
-	z01.PrintRune('\n')
-	piscine.PrintNbrBase(125, "aa")
-	z01.PrintRune('\n')
+	piscine.PrintNbrInOrder(321)
+	piscine.PrintNbrInOrder(0)
+	piscine.PrintNbrInOrder(321)
 }
