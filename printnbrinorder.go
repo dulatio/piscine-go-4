bfd88b547ba200c4aad0bbee9696829b082b5e25
cocozohan <dos93@mail.ru>
2019-10-22 11:21:01 +0600

    missing zero

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -6,11 +6,11 @@ func PrintNbrInOrder(n int) {
 	var ar []int
 	count := 0
 	for ; n > 0; n /= 10 {
-		ar = append(ar, n % 10)
+		ar = append(ar, n%10)
 		count++
 	}
 	for i := 1; i < count; i++ {
-		for j := 0; j < count - i; j++ {
+		for j := 0; j < count-i; j++ {
 			if ar[j] > ar[j+1] {
 				ar[j], ar[j+1] = ar[j+1], ar[j]
 			}
