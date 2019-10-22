b306bc780c20f66333c67f3e40b563463ee9e930
CocoZohan <dos93@mail.ru>
2019-10-22 14:35:14 +0600

    formatting

--- index.go
+++ index.go
@@ -1,20 +1,30 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	index := -1
-	rtofind := firstRune(toFind)
-	for _, r := range s {
-		index++
-		if r == rtofind {
-			return index
+	ar1 := []rune(s)
+	ar2 := []rune(toFind)
+	count1 := 0
+	for _, r := range ar1 {
+		count1++
+		_ = r
+	}
+	count2 := 0
+	for _, r := range ar2 {
+		count2++
+		_ = r
+	}
+	for i := 0; i < count1; i++ {
+		j, k := i, 0
+		for ; j < count1 && k < count2; {
+			if ar1[j] != ar2[k] {
+				break
+			}
+			j++
+			k++
+		}
+		if k == count2 {
+			return i
 		}
 	}
 	return -1
-}
-
-func firstRune(s string) rune {
-	for _, r := range s {
-		return r
-	}
-	return '\x00'
 }
