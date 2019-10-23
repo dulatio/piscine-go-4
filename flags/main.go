f95dd865641657de303905802dc1dc246df4e068
CocoZohan <dos93@mail.ru>
2019-10-23 16:12:28 +0600

    first

--- flags/main.go
+++ flags/main.go
@@ -13,14 +13,41 @@ func main() {
 		fmt.Println("--order")
 		fmt.Println("\t-o")
 		fmt.Println("\t\tThis flag will behave like a boolean, if it is called it will order the argument.")
+		return
 	}
 	toinsert := ""
 	order := false
-	for i, v := range os.Args {
-		if (len(v) > 9 && v[:9] == "--insert=") || (len) {
+	s := ""
+	for _, v := range os.Args {
+		if len(v) > 9 && v[:9] == "--insert=" {
 			toinsert = v[9:]
-		} else if len(v) == 7 && v == "--order" {
+		} else if len(v) > 2 && v[:3] == "-i=" {
+			toinsert = v[3:]
+		} else if v == "--order" || v == "-o" {
 			order = true
+		} else if v == "-h" || v == "--help" {
+			fmt.Println("--insert")
+			fmt.Println("\t-i")
+			fmt.Println("\t\tThis flag inserts the string into the string passed as argument.")
+			fmt.Println("--order")
+			fmt.Println("\t-o")
+			fmt.Println("\t\tThis flag will behave like a boolean, if it is called it will order the argument.")
+			return
+		} else {
+			s = v
 		}
 	}
+	s += toinsert
+	if order {
+		runes := []rune(s)
+		for i := 0; i < len(runes); i++ {
+			for j := 0; j < len(runes)-1-i; j++ {
+				if runes[j] > runes[j+1] {
+					runes[j], runes[j+1] = runes[j+1], runes[j]
+				}
+			}
+		}
+		s = string(runes)
+	}
+	fmt.Println(s)
 }
